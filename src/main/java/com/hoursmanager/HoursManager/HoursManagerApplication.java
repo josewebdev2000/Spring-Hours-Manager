7c45598bd8f2dea6b98c089dabcaf091076b3601
josewebdev2000 <josewebdev@proton.me>
2024-08-16 22:09:34 -0400

    first commit

---
+++ src/main/java/com/hoursmanager/HoursManager/HoursManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hoursmanager.HoursManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoursManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoursManagerApplication.class, args);
+	}
+
+}
