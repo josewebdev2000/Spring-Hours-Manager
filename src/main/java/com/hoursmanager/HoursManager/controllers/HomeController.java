afdd4b86bf1f40615e3b0e220a199acbe182d8db
josewebdev2000 <josewebdev@proton.me>
2024-08-17 01:05:45 -0400

    Configured Head for Home Page

--- src/main/java/com/hoursmanager/HoursManager/controllers/HomeController.java
+++ src/main/java/com/hoursmanager/HoursManager/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.hoursmanager.HoursManager.controllers;
 
+import com.hoursmanager.HoursManager.config.AppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,10 +9,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController
 {
+    @Autowired
+    private AppConfig appConfig;
+
     // Set GET Route for Home Page (index.html)
     @GetMapping("/")
     public String index(Model model)
     {
+        // Add Dynamic data to template
+        model.addAttribute("baseUrl", appConfig.getBaseUrl());
+        model.addAttribute("pageTitle", "Home");
+        model.addAttribute("pageDescription", "Hours Manager Web Application Home Page");
+        model.addAttribute("pageKeywords", "working, hours, tracker, budget, calculations, manager");
+        model.addAttribute("pageUrl", appConfig.getBaseUrl() + "/");
+
         return "index";
     }
 }
